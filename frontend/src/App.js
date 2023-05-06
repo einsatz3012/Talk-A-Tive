10628724382af87229450a1dc0d68f0c622c8d0f
einsatz3012 <rp707817@gmail.com>
2023-05-06 13:47:42 +0530

    deployment-fix axios-default

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,9 @@ import "./App.css";
 import { Route } from "react-router-dom";
 import ChatPage from "./Pages/ChatPage";
 import HomePage from "./Pages/HomePage";
+import axios from "axios";
+
+axios.defaults.baseURL = "https://talkative-backend-3qaa.onrender.com";
 
 function App() {
   return (
